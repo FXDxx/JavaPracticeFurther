f3a5fbfc5690f1aa142262efb49d0f7ab29329b1
Faizan <mfaizan33198@gmai.com>
2024-02-01 08:02:54 +0500

    2nd file uploaded

---
+++ javaPractice/SalaryCalculate.java
@@ -0,0 +1,28 @@
+package javaPractice;
+import java.util.*;
+
+public class SalaryCalculate {
+    double basicSalary;
+    double bonus;
+    double incomeTax;
+    double grossSalary;
+    public void calculateGrossSalary(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Salary: ");
+        basicSalary = sc.nextDouble();
+        System.out.println("Bonus: ");
+        bonus = sc.nextDouble();
+        grossSalary = basicSalary+bonus;
+        System.out.println(grossSalary);
+    }
+
+    public void calculateNetSalary(){
+        double netSalary = grossSalary-((2.5/100) * basicSalary);
+        System.out.println(netSalary);
+    }
+    public static void main(String[] args) {
+        SalaryCalculate s = new SalaryCalculate();
+        s.calculateGrossSalary();
+        s.calculateNetSalary();
+    }
+}
